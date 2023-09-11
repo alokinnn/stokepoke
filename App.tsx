6ca59a0644889076803a3afdcdbca17f4672c2e3
Nikola <nikfilipovic.19.08@gmail.com>
2023-09-11 21:39:41 +0200

    Setup minimal navigation structures

--- App.tsx
+++ App.tsx
@@ -4,15 +4,18 @@ import React from "react";
 import {persistor, store} from "./store/store";
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+          <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
